c91ac9e36005cab92a42b51ae72cececa78d64e5
ricku81 <92472490+ricku81@users.noreply.github.com>
2021-10-26 17:13:28 -0500

    Updated Version of the js File

--- connect4.js
+++ connect4.js
@@ -17,12 +17,12 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 const makeBoard = () => {
 	// set "board" to empty HEIGHT x WIDTH matrix array
-	const newArr = [];
-	for (let x = 0; x < WIDTH; x++) {
-		newArr.push(null);
-	}
 	for (let y = 0; y < HEIGHT; y++) {
+		const newArr = [];
 		board.push(newArr);
+		for (let x = 0; x < WIDTH; x++) {
+			newArr.push(null);
+		}
 	}
 };
 
@@ -61,31 +61,35 @@ const makeHtmlBoard = () => {
 
 const findSpotForCol = (x) => {
 	// TODO: write the real version of this, rather than always returning 0
-	let num = 1;
-	let y = board.length - num;
-	if (y !== null) {
-		num++;
+	let y = HEIGHT - 1;
+
+	if (board[y][x] !== null) {
+		y--;
 	}
+	console.log(`y = ${y}`);
+	console.log(board[y][x]);
 	return y;
 };
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 const placeInTable = (y, x) => {
-	// TODO: make a div and insert into correct table cell
+	// make a div and insert into correct table cell
 	const pieceSpot = document.getElementById(`${y}-${x}`);
 	const piece = document.createElement('div');
 
 	piece.classList.add(`piece`);
 	piece.classList.add(`p${currPlayer}`);
 
+	// remove this console.log
+	console.log(`${y}-${x}`);
 	pieceSpot.append(piece);
 };
 
 /** endGame: announce game end */
 
 const endGame = (msg) => {
-	// TODO: pop up alert message
+	// pop up alert message
 	alert(msg);
 };
 
@@ -96,6 +100,7 @@ const handleClick = (evt) => {
 	let x = +evt.target.id;
 
 	// get next spot in column (if none, ignore click)
+	// Do we need x in ()?
 	let y = findSpotForCol(x);
 	if (y === null) {
 		return;
@@ -103,7 +108,7 @@ const handleClick = (evt) => {
 
 	// place piece in board and add to HTML table
 	placeInTable(y, x);
-	// board[y][x] = currPlayer;
+	board[y][x] = currPlayer;
 
 	// check for win
 	if (checkForWin()) {
@@ -111,7 +116,11 @@ const handleClick = (evt) => {
 	}
 
 	// check for tie
-	// TODO: check if all cells in board are filled; if so call, call endGame
+	// check if all cells in board are filled; if so call endGame
+	const everyArrLoop = board.every((arr) => arr.every((val) => val !== null));
+	if (everyArrLoop === true) {
+		return endGame('Tie!');
+	}
 
 	// switch players
 	currPlayer === 1 ? (currPlayer = 2) : (currPlayer = 1);
@@ -125,92 +134,17 @@ const checkForWin = () => {
 		//  - cells: list of four (y, x) cells
 		//  - returns true if all are legal coordinates & all match currPlayer
 
-		return cells.every(
-			(
-				[
-					y,
-					x
-				]
-			) => y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && board[y][x] === currPlayer
-		);
+		return cells.every(([ y, x ]) => y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && board[y][x] === currPlayer);
 	}
 
 	// TODO: read and understand this code. Add comments to help you.
 
 	for (let y = 0; y < HEIGHT; y++) {
 		for (let x = 0; x < WIDTH; x++) {
-			const horiz = [
-				[
-					y,
-					x
-				],
-				[
-					y,
-					x + 1
-				],
-				[
-					y,
-					x + 2
-				],
-				[
-					y,
-					x + 3
-				]
-			];
-			const vert = [
-				[
-					y,
-					x
-				],
-				[
-					y + 1,
-					x
-				],
-				[
-					y + 2,
-					x
-				],
-				[
-					y + 3,
-					x
-				]
-			];
-			const diagDR = [
-				[
-					y,
-					x
-				],
-				[
-					y + 1,
-					x + 1
-				],
-				[
-					y + 2,
-					x + 2
-				],
-				[
-					y + 3,
-					x + 3
-				]
-			];
-			const diagDL = [
-				[
-					y,
-					x
-				],
-				[
-					y + 1,
-					x - 1
-				],
-				[
-					y + 2,
-					x - 2
-				],
-				[
-					y + 3,
-					x - 3
-				]
-			];
+			const horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
+			const vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
+			const diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
+			const diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
 
 			if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
 				return true;
@@ -221,9 +155,3 @@ const checkForWin = () => {
 
 makeBoard();
 makeHtmlBoard();
-
-// TODO List!
-// fix makeBoard function
-// fix findSpotForCol
-// Add a check for “is the entire board filled”
-// [hint: the JS every method on arrays would be especially nice here!]
